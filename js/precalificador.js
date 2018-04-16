e59fc5c6bf2e349ee945f331e352a6f7db2b20ca
Manuel Comi <mcomi@hotmail.com>
2018-04-16 11:46:23 -0500

    anotaciones UI

--- js/precalificador.js
+++ js/precalificador.js
@@ -20,3 +20,42 @@ $(".code-input").bind('keyup', function() {
     indexInput++
   }
 });
+
+var inputsText = document.querySelectorAll('input')
+
+inputsText.forEach(input => input.addEventListener('change', function() {
+  if (this.value !== '')
+    this.classList.add('valid')
+}))
+
+const inputsSolicitud = $('input')
+inputsSolicitud.each(function() {
+  let input = $(this)
+  input.change(function() {
+    if (input.val() !== '') {
+      if (input.attr('id') === 'celular') { // pregunto cuando sea el campo del celular
+        let regex = /^\(?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
+        if (regex.test(input.val())) { // valido el telefono
+          input.addClass('valid')
+          input.siblings('.input-success').html(input.val());
+
+        } else {
+          input.siblings('.input-error').html('No es un número de teléfono válido');
+          input.addClass('invalid')
+        }
+      } else {
+
+        input.addClass('valid')
+        input.siblings('.input-success').html(input.val());
+      }
+    }
+  })
+})
+
+// formatea el campo de telefono
+
+
+  let cleave = new Cleave('#celular', {
+    phone: true,
+    phoneRegionCode: 'MX'
+  });
